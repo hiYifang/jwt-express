1f29c820901faf527d2a20245ccd3d3390838ba1
hiYifang <a6426320@gmail.com>
2022-05-18 14:10:03 +0800

    feat: 新增身份驗證的 middleware

---
+++ service/auth.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const User = require('../models/usersModel');
+
+const appError = require('../service/appError');
+const handleErrorAsync = require('../service/handleErrorAsync');
+
+const jwt = require('jsonwebtoken');
+
+const isAuth = handleErrorAsync(async (req, res, next) => {
+  // 確認 token 是否存在
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
+    token = req.headers.authorization.split(' ')[1];
+  }
+
+  if (!token) {
+    return next(appError(401, '請輸入帳號密碼進行登入！', next));
+  }
+
+  // 驗證 token 正確性
+  const decoded = await new Promise((resolve, reject) => {
+    jwt.verify(token, process.env.JWT_SECRET, (err, payload) => {
+      if (err) {
+        reject(err)
+      } else {
+        resolve(payload)
+      }
+    })
+  })
+
+  const currentUser = await User.findById(decoded.id);
+  req.user = currentUser; // 自訂屬性，傳到下一個 middleware
+  next();
+});
+
+const generateSendJWT= (user,statusCode,res)=>{
+  // 產生 JWT token
+  const token = jwt.sign({id:user._id},process.env.JWT_SECRET,{
+    expiresIn: process.env.JWT_EXPIRES_DAY
+  });
+  user.password = undefined;
+  res.status(statusCode).json({
+    status: 'success',
+    user:{
+      token: token,
+      name: user.name
+    }
+  });
+}
+
+module.exports = {
+  isAuth,
+  generateSendJWT
+}
+
