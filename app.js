ec782615f62f0e8e70aa8d87b1e535a7f00c8ae1
hiYifang <a6426320@gmail.com>
2022-05-25 23:33:52 +0800

    fix: 修訂 app.js

--- app.js
+++ app.js
@@ -21,14 +21,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/posts', postsRouter);
 app.use('/users', usersRouter);
 
-// HTTP 狀態碼：404
-app.use(function (req, res, next) {
-  res.status(404).send('抱歉，您的頁面找不到');
-})
-
 /* 錯誤處理 */
 require('./service/process');
-const { errorHandlerMainProcess } = require('./service/errorHandler');
+const { appError, errorHandlerMainProcess } = require('./service/errorHandler');
+app.use((req, res, next) => { // HTTP 狀態碼：404
+  next(appError(404, '路由錯誤', '抱歉，您的頁面找不到'));
+});
 app.use(errorHandlerMainProcess);
 
 module.exports = app;
